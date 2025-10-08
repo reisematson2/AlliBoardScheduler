38c3dfd2fbf79416ab71c25ac09113415cfa2260
reisematson2 <reisematson2@gmail.com>
2025-10-08 21:17:55 +0000

    Set base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: '',
   plugins: [
     react(),
     // Only include Replit plugins in development and when REPL_ID is present
